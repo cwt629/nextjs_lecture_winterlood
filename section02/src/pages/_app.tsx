1a8d3139384d8385ecd8d773fd80694d191d44a8
Wontae Chang <cwt629@naver.com>
2025-06-10 18:40:44 +0900

    20250610 section2-4까지 완료

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,10 +1,37 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/test"); // Link 컴포넌트들은 pre-fetching이 되지만, programmatic하게 이동하는 이런 경우는 안 됨.
+    // router.replace("/test");
+    // router.back();
+  };
+
+  // programmatic한 링크도 프리페칭하기: 명시적으로
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
-      <header>글로벌 헤더</header>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        {/* pre-fetching 해제하기 */}
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+        <div>
+          <button onClick={onClickButton}>/test 페이지로 이동</button>
+        </div>
+      </header>
       <Component {...pageProps} />
     </>
   );
